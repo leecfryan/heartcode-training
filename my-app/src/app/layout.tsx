95c96af45884d279d9e2a04ba587f82c296731aa
leecfryan <61502605+leecfryan@users.noreply.github.com>
2024-10-11 16:35:40 +0800

    test

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -4,6 +4,8 @@ import { NavigationBar } from "./components/navbar/navigation-menu";
 import { ThemeProvider } from "./components/theme-provider";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -41,6 +43,14 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <NavigationBar />
+            <div>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
             {children}
             <Toaster />
           </ThemeProvider>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,6 +4,7 @@ import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import Link from "next/link";
 
 import MyInfiniteScroll from "./components/MyInfiniteScroll/MyInfiniteScroll";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const words: string[] = ["Alcohol", "Drugs"];
@@ -14,7 +15,9 @@ export default function Home() {
         <div className="flex flex-col items-center">
           <div className="font-bold text-center text-5xl uppercase">
             DON'T DO
-            <span><FlipWords words={words} /></span>
+            <span>
+              <FlipWords words={words} />
+            </span>
           </div>
           <div className="mt-5 hover:scale-150 transition ease-in-out">
             <HoverBorderGradient
@@ -30,7 +33,9 @@ export default function Home() {
 
       <div className="flex flex-col pt-20 justify-center px-7 h-full bg-gradient-to-t from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800">
         <div className="flex flex-col h-full" id="drug_types">
-          <h1 className="font-sans antialiased text-center text-2xl font-bold">Drug Types</h1>
+          <h1 className="font-sans antialiased text-center text-2xl font-bold">
+            Drug Types
+          </h1>
           <MyInfiniteScroll />
         </div>
       </div>
